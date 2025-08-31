df19a37090dd6032bceca797c888aa46c691133f
chope <100432481@alumnos.uc3m.es>
2025-09-01 00:31:54 +0200

    Add more verbosity and support to multiple package managers

--- setup.sh
+++ setup.sh
@@ -3,69 +3,127 @@
 USER=$1
 HOME=/home/${USER}
 
-function init_setup() {
-  sudo cp -r .tmux.conf .bashrc .config/ ${HOME} 2>/dev/null
+# Detect package manager
+detect_pkg_manager() {
+  if command -v apt &>/dev/null; then
+    PKG_MANAGER="apt"
+  elif command -v dnf &>/dev/null; then
 
-  cd ${HOME} 
-  echo "Moving home $(pwd)"
-
-  tmux source-file ${HOME}/.tmux.conf 2>/dev/null
-  source ${HOME}/.bashrc
-  return 0
+    PKG_MANAGER="dnf"
+  elif command -v yum &>/dev/null; then
+    PKG_MANAGER="yum"
+  elif command -v pacman &>/dev/null; then
+    PKG_MANAGER="pacman"
+  else
+    echo "Unsupported package manager. Exiting."
+    exit 1
+  fi
 }
 
-function install_basics() {
-  echo "--------------------- Installing Core utilities ------------------------------"
-  sudo apt update
-
-  sudo apt install python3 ninja-build cmake -y
-  sudo apt install unzip curl git build-essential gettext lua5.4 fish -y
-  return 0
+install_packages() {
+  echo "--------------------- Installing core utilities ------------------------------"
+  case "$PKG_MANAGER" in
+  apt)
+    sudo apt update
+    sudo apt install -y python3 ninja-build cmake ripgrep unzip curl git build-essential gettext lua5.1 fish
+    ;;
+  dnf | yum)
+    sudo $PKG_MANAGER install -y python3 ninja-build cmake ripgrep unzip curl git gcc gcc-c++ make gettext lua fish
+    ;;
+  pacman)
+    sudo pacman -Sy --noconfirm python ninja cmake ripgrep unzip curl git base-devel gettext lua fish
+    ;;
+  esac
 }
 
-function install_neovim() {
+init_setup() {
+  echo "--------------------- Setting up user environment for ${USER} ------------------------------"
+  cp -r .tmux.conf .bashrc .config/ "${HOME}/" 2>/dev/null
+
+  sudo chown -R "${USER}:${USER}" "${HOME}/.tmux.conf" "${HOME}/.bashrc" "${HOME}/.config"
+  echo "Configuration files successfully copied to ${HOME} (owned by ${USER})"
+}
+
+install_neovim() {
   echo "--------------------- Installing Neovim ------------------------------"
-  mkdir downloads/
-  cd downloads/
-    if command -v nvim &> /dev/null; then
+  if command -v nvim &>/dev/null; then
 
-  curl -LO "https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.tar.gz" &&
-    tar xvf nvim-linux-x86_64.tar.gz &&
-    mv cp nvim-linux-x86_64/bin/nvim /usr/bin/
-    nvim --version
+    NVIM_VER=$(nvim --version | head -n1 | awk '{print $2}')
+    if [ "$(printf '%s\n' '0.11.0' "$NVIM_VER" | sort -V | head -n1)" = "0.11.0" ]; then
+
+      echo "Neovim $NVIM_VER is already installed. Skipping installation."
+      return 0
     fi
+  fi
 
-    
-  echo "--------------------- Installing Lazygit ------------------------------"
-    if command -v lazygit &> /dev/null; then
-  curl -LO "https://github.com/jesseduffield/lazygit/releases/download/v0.54.2/lazygit_0.54.2_linux_x86_64.tar.gz"
-    tar xfv "lazygit_0.54.2_linux_x86_64.tar.gz" &&
-	mv lazygit /usr/bin && 
-    lazygit --version
+  mkdir -p downloads && cd downloads || exit
+  curl -LO "https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.tar.gz"
+  tar xvf nvim-linux-x86_64.tar.gz
 
-    fi
-  cd ..
-  rm -rf downloads/
-
-  return 0
+  sudo rm -rf /opt/nvim-linux-x86_64/
+  sudo mv nvim-linux-x86_64/ /opt/
+  sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+  echo "Neovim installed: $(nvim --version | head -n1)"
+  cd .. && rm -rf downloads
 }
 
-function install_languages() {
-  echo "--------------------- Installing programming languages ------------------------------"
+install_lazygit() {
 
-  # C++
-  sudo apt install -y clang gcc g++ gdb
+  echo "--------------------- Installing Lazygit ------------------------------"
+  if command -v lazygit &>/dev/null; then
+    echo "Lazygit $(lazygit --version) is already installed. Skipping installation."
+    return 0
+  fi
+
+  mkdir -p downloads && cd downloads || exit
+  curl -LO "https://github.com/jesseduffield/lazygit/releases/download/v0.54.2/lazygit_0.54.2_linux_x86_64.tar.gz"
+  tar xvf lazygit_0.54.2_linux_x86_64.tar.gz
+
+  sudo install lazygit /usr/local/bin
+
+  echo "Lazygit installed: $(lazygit --version)"
+  cd .. && rm -rf downloads
+}
+
+install_languages() {
+  echo "--------------------- Installing programming languages ------------------------------"
+  case "$PKG_MANAGER" in
+  apt)
+    sudo apt install -y clang gcc g++ gdb
+    ;;
+  dnf | yum)
+    sudo $PKG_MANAGER install -y clang gcc gcc-c++ gdb
+    ;;
+  pacman)
+    sudo pacman -Sy --noconfirm clang gcc gdb
+    ;;
+  esac
 
   # Go
-  sudo rm -rf /usr/local/go
-  sudo tar -C /usr/local -xzf go1.25.0.linux-amd64.tar.gz
+  echo "Installing Go..."
+  if [ -f go1.25.0.linux-amd64.tar.gz ]; then
+    sudo rm -rf /usr/local/go
+    sudo tar -C /usr/local -xzf go1.25.0.linux-amd64.tar.gz
+    echo "Go installed successfully."
+  else
+    echo "Go tarball not found. Skipping Go installation."
+
+  fi
 
   # Rust
+  echo "Installing Rust..."
   curl https://sh.rustup.rs -sSf | sh -s -- -y
-  source $HOME/.cargo/env
-
-  return 0
+  sudo -u $USER source $HOME/.cargo/env
+  echo "Rust installed successfully."
 }
 
+# Main
+detect_pkg_manager
+install_packages
 init_setup
 
+install_neovim
+install_lazygit
+install_languages
+
+echo "--------------------- Setup complete! ------------------------------"
