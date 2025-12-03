b90fed0fed9382a6dcd9fb5deccbf18399ef06dd
Alvaro Cabrera Barrio <alvaro.cabrera@six-group.com>
2025-12-03 15:40:50 +0100

    Add generic shell task to overseer

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -1,35 +1,24 @@
 return {
   {
     "stevearc/overseer.nvim",
-    opts = {}, -- Puedes poner opciones globales aquí
+    opts = {},
     config = function(_, opts)
       local overseer = require("overseer")
       overseer.setup(opts)
 
-      -- === 1. Tarea CMake Configure ===
       overseer.register_template({
         name = "CMake Configure",
         desc = "Generar archivos de construcción en ./build",
-
         params = {
-          mi_arg = {
-            type = "string",
-            desc = "cmake [...]",
-            optional = false,
-          },
+          mi_arg = { type = "string", desc = "cmake [...]", optional = false },
         },
         builder = function(params)
           return {
-
             cmd = { "cmake" },
             args = { params.mi_arg },
-            components = {
-              "default",
-              "on_complete_notify",
-            },
+            components = { "default", "on_complete_notify" },
           }
         end,
-
         condition = {
           callback = function(search)
             return vim.fn.filereadable(search.dir .. "/CMakeLists.txt") == 1
@@ -39,32 +28,49 @@ return {
 
       overseer.register_template({
         name = "Run CTest",
-
         desc = "Ejecutar tests dentro de ./build",
         params = {
-          mi_arg = {
-            type = "string",
-            desc = "ctest [...]",
-            optional = false,
-          },
+          mi_arg = { type = "string", desc = "ctest [...]", optional = false },
         },
-
         builder = function(params)
           return {
             cmd = { "ctest" },
             args = { params.mi_arg },
-
             cwd = "build",
-
-            components = {
-              "default",
-              "on_complete_notify",
-            },
+            components = { "default", "on_complete_notify" },
           }
         end,
         condition = {
           callback = function(search)
             return vim.fn.isdirectory(search.dir .. "/build") == 1
+          end,
+        },
+      })
+
+      overseer.register_template({
+        name = "Shell Command",
+        desc = "Ejecuta cualquier comando manual en la terminal",
+        params = {
+          cmd = {
+            type = "string",
+            desc = "Comando a ejecutar (ej: ls -la | grep x)",
+            optional = false,
+          },
+        },
+        builder = function(params)
+          return {
+            cmd = vim.o.shell,
+            args = { "-c", params.cmd },
+            components = {
+              "default",
+              "on_complete_notify",
+              "unique",
+            },
+          }
+        end,
+        condition = {
+          callback = function()
+            return true
           end,
         },
       })
