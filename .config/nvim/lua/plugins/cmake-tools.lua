6b5c90a7e17a3cd3432d59834969e515a1a7f3ad
ch0pe <acbsur1@gmail.com>
2026-01-04 22:57:53 +0100

    Add copilot to nvim plugins

--- .config/nvim/lua/plugins/cmake-tools.lua
+++ .config/nvim/lua/plugins/cmake-tools.lua
@@ -1,63 +1,3 @@
--- return {
---   {
---     "Civitasv/cmake-tools.nvim",
---     config = function()
---       local cmake = require("cmake-tools")
---       local dap = require("dap")
---
---       cmake.setup({
---         cmake_command = "cmake",
---         cmake_build_directory = "", -- carpeta de build raíz
---         cmake_build_directory_prefix = "build/", -- prefijo de carpetas de build
---         cmake_build_type = "Debug", -- build type por defecto
---         cmake_generate_options = { "-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1" },
---         cmake_build_options = {},
---         cmake_console_size = 10,
---         cmake_dap_configuration = {
---           name = "cpp",
---           type = "codelldb",
---
---           request = "launch",
---           cwd = "${workspaceFolder}",
---
---           stopOnEntry = false,
---           runInTerminal = true,
---           console = "integratedTerminal",
---         },
---         cmake_dap_open_command = dap.repl.open, -- opcional
---         cmake_variants_message = {
---           short = { show = true },
---           long = { show = true, max_length = 40 },
---         },
---         -- Añadimos la configuración de terminal con nombre
---         terminal = {
---           split_direction = "vertical",
---           split_size = 11,
---           single_terminal_per_instance = false,
---           single_terminal_per_tab = false,
---           keep_terminal_static_location = false,
---           auto_resize = true,
---
---           focus = true,
---           do_not_add_newline = false,
---         },
---       })
---
---       local opts = { noremap = true, silent = true }
---
---       -- Keymaps rápidos
---       vim.keymap.set("n", "<leader>jb", "<cmd>CMakeBuild<cr>", opts)
---       vim.keymap.set("n", "<leader>jr", "<cmd>CMakeRun<cr>", opts)
---       vim.keymap.set("n", "<leader>jd", "<cmd>CMakeDebug<cr>", opts)
---       vim.keymap.set("n", "<leader>js", "<cmd>CMakeSelectBuildTarget<cr>", opts)
---       vim.keymap.set("n", "<leader>jl", "<cmd>CMakeSelectLaunchTarget<cr>", opts)
---       vim.keymap.set("n", "<leader>jk", "<cmd>CMakeSelectKit<cr>", opts)
---       vim.keymap.set("n", "<leader>jc", "<cmd>CMakeSelectConfigurePreset<cr>", opts)
---       vim.keymap.set("n", "<leader>jp", "<cmd>CMakeSelectBuildPreset<cr>", opts)
---       vim.keymap.set("n", "<leader>jt", "<cmd>CMakeRunTest<cr>", opts)
---     end,
---   },
--- }
 return {
   {
     "Civitasv/cmake-tools.nvim",
@@ -71,8 +11,8 @@ return {
 
       cmake.setup({
         cmake_command = "cmake",
-        ctest_command = "ctest",
-        cmake_build_options = {},
+        ctest_command = "ctest --preset Debug-linux-gcc",
+        cmake_regenerate_on_save = false,
         cmake_dap_configuration = {
           name = "cpp",
           type = "codelldb",
@@ -81,36 +21,41 @@ return {
           runInTerminal = true,
           console = "integratedTerminal",
         },
-        cmake_executor = { -- executor to use
-          name = "quickfix", -- name of the executor
-
-          opts = {}, -- the options the executor will get, possible values depend on the executor type. See `default_opts` for possible values.
-
-          default_opts = { -- a list of default and possible values for executors
-            overseer = {},
-            quickfix = {
-              show = "always", -- "always", "only_on_error"
-              position = "vertical", -- "vertical", "horizontal", "leftabove", "aboveleft", "rightbelow", "belowright", "topleft", "botright", use `:h vertical` for example to see help on them
-
-              size = 90,
-              encoding = "utf-8", -- if encoding is not "utf-8", it will be converted to "utf-8" using `vim.fn.iconv`
-              auto_close_when_success = true, -- typically, you can use it with the "always" option; it will auto-close the quickfix buffer if the execution is successful.
+        cmake_executor = {
+          name = "overseer",
+          default_opts = {
+            overseer = {
+              new_task_opts = {
+                strategy = {
+                  "jobstart",
+                  use_terminal = false,
+                  perserve_output = false,
+                },
+              },
+              -- if you don't want the overseer task list to open overwritting this
+              -- function helps. Otherwise just remove this section
+              on_new_task = function(task)
+                require("overseer").open({ enter = true, direction = "bottom" })
+              end,
             },
           },
         },
         cmake_runner = { -- runner to use
-          name = "toggleterm", -- name of the runner
-          opts = {}, -- the options the runner will get, possible values depend on the runner type. See `default_opts` for possible values.
-
-          default_opts = { -- a list of default and possible values for runners
-            overseer = {},
-            quickfix = {
-              show = "always", -- "always", "only_on_error"
-              position = "vertical", -- "bottom", "top"
-
-              size = 90,
-              encoding = "utf-8",
-              -- auto_close_when_success = true, -- typically, you can use it with the "always" option; it will auto-close the quickfix buffer if the execution is successful.
+          name = "overseer",
+          default_opts = {
+            overseer = {
+              new_task_opts = {
+                strategy = {
+                  "jobstart",
+                  use_terminal = false,
+                  perserve_output = false,
+                },
+              },
+              -- if you don't want the overseer task list to open overwritting this
+              -- function helps. Otherwise just remove this section
+              on_new_task = function(task)
+                require("overseer").open({ enter = true, direction = "bottom" })
+              end,
             },
           },
         },
--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -1,79 +1,33 @@
 return {
   {
     "stevearc/overseer.nvim",
-    opts = {},
+    keys = {
+      {
+        "<leader>os",
+        function()
+          vim.ui.input({ prompt = "Command: " }, function(input)
+            if input and input ~= "" then
+              require("overseer")
+                .new_task({
+                  cmd = vim.split(input, " "),
+                  components = { "default" },
+                })
+                :start()
+            end
+          end)
+        end,
+        desc = "Run Manual Command",
+      },
+    },
+    opts = {
+      task_list = {
+        direction = "bottom",
+        min_height = 30,
+      },
+    }, -- Puedes poner opciones globales aquí
     config = function(_, opts)
       local overseer = require("overseer")
       overseer.setup(opts)
-
-      overseer.register_template({
-        name = "CMake Configure",
-        desc = "Generar archivos de construcción en ./build",
-        params = {
-          mi_arg = { type = "string", desc = "cmake [...]", optional = false },
-        },
-        builder = function(params)
-          return {
-            cmd = { "cmake" },
-            args = { params.mi_arg },
-            components = { "default", "on_complete_notify" },
-          }
-        end,
-        condition = {
-          callback = function(search)
-            return vim.fn.filereadable(search.dir .. "/CMakeLists.txt") == 1
-          end,
-        },
-      })
-
-      overseer.register_template({
-        name = "Run CTest",
-        desc = "Ejecutar tests dentro de ./build",
-        params = {
-          mi_arg = { type = "string", desc = "ctest [...]", optional = false },
-        },
-        builder = function(params)
-          return {
-            cmd = { "ctest" },
-            args = { params.mi_arg },
-            cwd = "build",
-            components = { "default", "on_complete_notify" },
-          }
-        end,
-        condition = {
-          callback = function(search)
-            return vim.fn.isdirectory(search.dir .. "/build") == 1
-          end,
-        },
-      })
-
-      overseer.register_template({
-        name = "Shell Command",
-        desc = "Ejecuta cualquier comando manual en la terminal",
-        params = {
-          cmd = {
-            type = "string",
-            desc = "Comando a ejecutar (ej: ls -la | grep x)",
-            optional = false,
-          },
-        },
-        builder = function(params)
-          return {
-            cmd = vim.o.shell,
-            args = { "-c", params.cmd },
-            components = {
-              "default",
-              "on_complete_notify",
-              "unique",
-            },
-          }
-        end,
-        condition = {
-          callback = function()
-            return true
-          end,
-        },
-      })
     end,
   },
 }
